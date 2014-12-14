782685f02f0d35ef9b81466660f79af7cd1b8f01
Matt Gauger <matt.gauger@gmail.com>
2014-12-13 18:38:08 -0600

    Remove Clojure dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "http://github.com/mathias/hum"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2411"]])
+  :dependencies [[org.clojure/clojurescript "0.0-2411"]])
